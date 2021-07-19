7952130572f0385b951a8367f1fcea0d1bddd4ff
Artur Rodrigues <tui-dtj@hotmail.com>
2021-07-19 11:08:18 -0300

    Adding a Input value check

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,12 @@ const emailInput = document.getElementById("email");
 
 submitInput.addEventListener("click", (e) => {
   e.preventDefault();
+
+  if(nameInput.value == "" || emailInput.value == "") {
+    confirm("Preencha os dados para finalizar o cadastro...")
+  }
+  else {
   localStorage.setItem("name", nameInput.value);
   localStorage.setItem("email", emailInput.value);
+  }
 });
-
